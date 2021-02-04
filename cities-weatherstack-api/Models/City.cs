8f4df80e9fd54f554319f2d2632cab6914a71776
Felipe <felipe.pereira.dev@gmail.com>
2021-02-04 10:40:34 -0300

    feat: db context with City model

---
+++ cities-weatherstack-api/Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace cities_weatherstack_api.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string CityName { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ cities-weatherstack-api/Models/Context.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cities_weatherstack_api.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<City> Cities { get; set; }
+    }
+}
