5e26b3280ceedf31ac8865dde373630adf5f8f14
Felipe <felipe.pereira.dev@gmail.com>
2021-02-04 10:47:05 -0300

    chore: connection string

--- cities-weatherstack-api/Startup.cs
+++ cities-weatherstack-api/Startup.cs
@@ -1,7 +1,9 @@
+using cities_weatherstack_api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,9 @@ namespace cities_weatherstack_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
